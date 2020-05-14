05d7464cf655d36f003a02ebdb9c9172c839757f
BisterNastic <bistercoco@gmail.com>
2020-05-14 16:18:55 +0000

    klhejrzeaze

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'host' => env('smtp.gmail.com', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('achrafbister@gmail.com'),
+            'password' => env('19960807aK'),
             'timeout' => null,
         ],
 
@@ -83,8 +83,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('achrafbister@gmail.com', 'hello@example.com'),
+        'name' => env('TASKS APP', 'Example'),
     ],
 
     /*
